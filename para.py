3f259df9dec7a16a14e32e2ef9e42c1d67038676
taeseokyang <ts.yang.0123@gmail.com>
2023-11-18 01:03:34 +0900

    fix: fix gragh function

--- para.py
+++ para.py
@@ -90,46 +90,50 @@ if __name__ == '__main__':
     ##################################
 
     # 이동 평균 관련 변수
-    window = 5
+    window = 10
     init_altitude = 0  # 기준 영점
-    threshold = 10 # 이상치 임계값
-    no_deploy_altitude = 3
+    threshold = 20 # 이상치 임계값
+    no_deploy_altitude = 1
     moving_averages = []
 
     falling_count = 0
     # 센서 초기화
     init_buffer = []
     init_times = 50
-    for i in range(init_times):init_buffer.append(bmp280.altitude)
+    for i in range(init_times):
+        print("Wait Initialing...")
+        init_buffer.append(bmp280.altitude)
     init_altitude = sum(init_buffer)/init_times
     datas = []
-    for i in range(20):
+    for i in range(window-1):
+        print("Wait Initialing...")
         datas.append(0.1)
     datas.append(0.2)
     #Bmp280
     while True:
         altitude = bmp280.altitude - init_altitude # 로컬 고도 계산
         # 이상치 탐지, Z-score기법 사용
-        mean = np.mean(datas[-20:])
-        std = np.std(datas[-20:])
+        mean = np.mean(datas[-window:])
+        std = np.std(datas[-window:])
         z = (altitude-mean)/std 
         print('Z-score : {:.2f}'.format(z))
         if z > threshold: 
             print('###########################################Detected outlier with : {:.2f}'.format(altitude))
         else: 
             datas.append(altitude) # 데이터 리스트에 추가
-            if len(datas)>20:
+            if len(datas)>window:
                 moving_averages.append(mean)
 
         # 낙하산 사출 조건 검사
-        if  len(moving_averages) > 5 and moving_averages[-5]>moving_averages[-1]: 
+        if  len(moving_averages) > 2 and moving_averages[-2]>moving_averages[-1]: 
             falling_count += 1
             print("DOWN", falling_count)
         else: 
             falling_count = 0
             print("UP", falling_count)
        
-        if falling_count>5 and moving_averages[-1] > no_deploy_altitude:
+        falling_confirmation = 3
+        if falling_count> falling_confirmation and moving_averages[-1] > no_deploy_altitude:
             try:
                 # 서보 동작
                 # pwm.ChangeDutyCycle(7.5)
@@ -147,13 +151,18 @@ if __name__ == '__main__':
         # 로컬 고도 출력
         print("Altitude: {:.2f}".format(altitude))
 
-        x = datas
-        y = range(len(datas))
-        plt.plot(x, y, color="red")
-        # plt.xlabel("Time(ms)")
-        # plt.ylabel("Newton")
-        # plt.title("Trapezoidal Roul on TMS")
-        plt.show()
+y = datas[20:]
+x = range(window//2,len(datas)-20+window//2)
+y2 = moving_averages
+x2 = range(len(moving_averages))
+plt.scatter(x2[-1],y2[-1],color="red", marker="*",s=500,label='Deploy!')
+plt.plot(x, y, color="red")
+plt.plot(x2,y2,color="blue")
+plt.axhline(y=no_deploy_altitude,color="green")
+plt.xlabel("Data Count")
+plt.ylabel("Altitude")
+plt.title("Parachute_deployment")
+plt.show()
         # 로컬 고도 데이터 저장
         # f.write(f"Calibration altitude: {cali_altitude} m\n")
 input()
